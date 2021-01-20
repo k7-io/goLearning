2447e15dd629d7ddb2a90ddb9d42b10bfff10ecf
tphyhFighting <tphyhFighting@github.com>
2021-01-20 16:36:42 +0800

    pkg manage and pkg test

---
+++ pkgManage/pkg.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	//remote import
+	//import github.com/gin-gonic/gin
+	//alias import
+	print "fmt"
+	date "time"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	//anonymous import
+	"net/http"
+	_ "net/http/pprof"
+	"pkgTest"
+)
+
+func ping(writer http.ResponseWriter, request *http.Request)  {
+	writer.Write([]byte("{\"message\":\"pong\"}"))
+}
+
+func main()  {
+	//r := gin.Default()
+	//r.GET("/ping", func(c *gin.Context){
+	//	c.JSON(200, gin.H{
+	//		"message" : "pong",
+	//	})
+	//})
+	//r.Run()
+
+	print.Println(pkgTest.SumNum)
+
+	//alias import use
+	print.Println("the current data is:", date.Now().Day())
+	print.Printf("second of time pkg: %v\n", date.Second)
+
+	http.HandleFunc("/ping", ping)
+	http.ListenAndServe(":8080", nil)
+
+	dbname := "db conn string"
+	db, err := sql.Open("mysql", dbname)
+	//operate db
+	_, _ = db, err
+}
---
+++ pkgTest/pkgPublic.go
@@ -0,0 +1,10 @@
+package pkgTest
+
+import "fmt"
+
+var SumNum = 1
+var firstName = "Peter"
+var isValid = false
+func printValue() {
+	fmt.Println("01 quick study!")
+}
