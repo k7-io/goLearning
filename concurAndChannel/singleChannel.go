f3fb329b1e2594e212cddbb63e9f49faaf97d02c
tphyhFighting <2363176358@qq.com>
2021-01-23 21:29:32 +0800

    update singleChannel:add time.NewTicker

--- concurAndChannel/singleChannel.go
+++ concurAndChannel/singleChannel.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func producer(out chan<- int)  {
 	for i := 0; i < 10; i++ {
@@ -19,4 +22,10 @@ func main()  {
 	ch := make(chan int)
 	go producer(ch)
 	consumer(ch)
+
+	ticker := time.NewTicker(time.Second)
+	for {
+		<-ticker.C
+		fmt.Println("loop")
+	}
 }
