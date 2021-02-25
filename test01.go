e81b5894c448fd3e795b557f81b72849b3a6b7bd
tphyhFighting <2363176358@qq.com>
2021-02-25 18:57:27 +0800

    add: gin redis queue in/out/len

--- test01.go
+++ test01.go
@@ -28,26 +28,78 @@ func dbTest() {
 	fmt.Printf("size:%v\n", size)
 }
 
+func HttpRedisLen(c *gin.Context) {
+	name := c.DefaultQuery("name", "myLen")
+	fmt.Println("HttpRedisLen name:", name)
+	size, err := db.Len(name)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"message": "success",
+		"size":    size,
+	})
+}
+
+func HttpRedisInQueue(c *gin.Context) {
+	name := c.DefaultQuery("name", "myTestInQueue")
+	eleList, has := c.GetQueryArray("element")
+	if has {
+		fmt.Printf("eleList:%v\n", eleList)
+	}
+	inData := make([]interface{}, len(eleList))
+	for i := 0; i < len(inData); i++ {
+		inData[i] = eleList[i]
+	}
+	err := db.InQueue(name, inData)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"message": "success",
+		"inData":  eleList,
+	})
+}
+
+func HttpRedisOutQueue(c *gin.Context) {
+	name := c.DefaultQuery("name", "myTestOutQueue")
+	res, err := db.OutQueue(name)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	var resStr []string
+	for i := 0; i < len(res); i++ {
+		resStr = append(resStr, string(res[i].([]uint8)))
+	}
+	c.JSON(200, gin.H{
+		"message": "success",
+		"outData": resStr,
+	})
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/redis/list/len", func(c *gin.Context) {
-		//name := c.DefaultQuery("name", "myLen")
-		nameList, has := c.GetQueryArray("name")
-		if has {
-			fmt.Printf("nameList:%v\n", nameList)
+
+	r.GET("/redis/list/:function", func(c *gin.Context) {
+		function := c.Param("function")
+		if function == "len" {
+			HttpRedisLen(c)
+		} else if function == "inQueue" {
+			HttpRedisInQueue(c)
+		} else if function == "outQueue" {
+			HttpRedisOutQueue(c)
+		} else {
+			c.String(404, "Error function %s", function)
 		}
-		name := "myLen"
-		size, err := db.Len(name)
-		if err != nil {
-			c.JSON(400, gin.H{
-				"message": err.Error(),
-			})
-			return
-		}
-		c.JSON(200, gin.H{
-			"message": "success",
-			"size":    size,
-		})
 	})
 	r.Run(":8000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
