d9220bfc8693c676b6d4edadffc8e8b04f7961d1
huoyinghui <yhhuo@yunjinginc.com>
2021-03-12 16:53:48 +0800

    add:ring (循环双链表)test do/move

--- test/lib/container/ring_test.go
+++ test/lib/container/ring_test.go
@@ -44,3 +44,61 @@ func TestRingReIter(t *testing.T)  {
 		t.Logf("value:%v\n", r.Value)
 	}
 }
+
+func TestRingDo(t *testing.T)  {
+	r := ring.New(4)
+	t.Log(r.Len())
+	n := r.Len()
+	// Initialize the ring with some integer values
+	for i := 0; i < n; i++ {
+		r.Value = i
+		r = r.Next()
+	}
+
+	r.Do(func(i interface{}) {
+		t.Logf("i:%v", i)
+	})
+
+	rr := ring.New(r.Len())
+	r.Do(func(i interface{}) {
+		// copy r ele to rr
+		rr.Value = i
+		rr = rr.Next()
+	})
+
+	for i:=0; i<rr.Len(); i++ {
+		t.Logf("rr:%v", rr.Value)
+		rr = rr.Next()
+	}
+
+}
+
+func TestRingMov(t *testing.T)  {
+	r := ring.New(4)
+	t.Log(r.Len())
+	n := r.Len()
+	// Initialize the ring with some integer values
+	for i := 0; i < n; i++ {
+		r.Value = i
+		r = r.Next()
+	}
+	t.Logf("first:%v", r.Value)
+	r = r.Move(3)
+	t.Logf("last:%v", r.Value)
+}
+
+func TestRingMovV2(t *testing.T)  {
+	r := ring.New(4)
+	t.Log(r.Len())
+	n := r.Len()
+	// Initialize the ring with some integer values
+	for i := 0; i < n; i++ {
+		r.Value = i
+		r = r.Next()
+	}
+	t.Logf("first:%v", r.Value)
+	index := 5
+	r = r.Move(index%n)
+	t.Logf("last:%v", r.Value)
+}
+// todo: link/unlink
