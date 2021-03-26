7341b653283a31615dd645f5e620301eb8a32237
huoyinghui <yhhuo@yunjinginc.com>
2021-03-26 14:09:57 +0800

    add: cache 使用redis 链接池 & 源cache.go => queue.go

--- cache/cache.go
+++ cache/cache.go
@@ -1,81 +1,56 @@
 package cache
+/*
+how to use:
+	1.CMInit()
+	2.c := GetCache
+	3.c.Close()
+*/
 
 import (
 	"github.com/gomodule/redigo/redis"
+	"time"
 )
 
-type DB struct {
-	client redis.Conn
-}
+var (
+	pool *redis.Pool
+)
 
-
-func (db *DB) Init(redisConf string) {
-	c, err := redis.Dial("tcp", redisConf)
-	if err != nil {
-		// handle error
-		panic("redis conn err:" + err.Error())
-	}
-	db.client = c
-	//defer c.Close()
-}
-
-func (db *DB) Close() {
-	if db.client != nil {
-		db.client.Close()
-	}
-}
-
-func (db *DB) InQueue(name string, values []interface{}) (err error) {
-	size := len(values)
-	for i := 0; i < size; i++ {
-		_, err := db.client.Do("RPUSH", name, values[i])
-		if err != nil {
+/*
+server: 192.168.0.100:26379
+password: ""
+when len(password) more than 0, will do auth
+*/
+func newPool(server, password string) *redis.Pool {
+	return &redis.Pool{
+		MaxIdle: 3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func () (redis.Conn, error) {
+			c, err := redis.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			if len(password) <= 0 {
+				return c, err
+			}
+			if _, err := c.Do("AUTH", password); err != nil {
+				c.Close()
+				return nil, err
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
 			return err
-		}
+		},
 	}
-	return nil
 }
 
-func (db *DB) Len(name string) (int, error) {
-	return redis.Int(db.client.Do("LLEN", name))
+func CMInit(server, password string) {
+	if pool == nil {
+		pool = newPool(server, password)
+	}
 }
 
-func (db *DB) OutQueueOne(name string) (value interface{}, err error) {
-	reply, err := db.client.Do("LPOP", name)
-	return reply, err
-	//return db.client.Do("LPOP", name)
-}
-
-func (db *DB) OutQueue(name string) (values []interface{}, err error) {
-	size, err := db.Len(name)
-	if err != nil {
-		return nil, err
-	}
-	for i := 0; i < size; i++ {
-		ele, err := db.OutQueueOne(name)
-		if err != nil {
-			return nil, err
-		}
-		values = append(values, ele)
-	}
-	return values, nil
-}
-
-func (db *DB) DelDBName(name string) (reply interface{}, err error) {
-	return db.client.Do("del", name)
-}
-
-func StringSliceEqual(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	if (a == nil) != (b == nil) {
-		return false
-	}
-	for i, v := range a {
-		if v != b[i] {
-			return false
-		}
-	}
-	return true
+func GetCache() redis.Conn{
+	return pool.Get()
 }
